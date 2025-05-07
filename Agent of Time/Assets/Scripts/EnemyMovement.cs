e272f1283e70bb5a1979caab807873d13771b9d1
MungyMug <144017801+MungyMug@users.noreply.github.com>
2025-05-08 05:18:16 +0800

    Pushing of enemy and player collision

--- Agent of Time/Assets/Scripts/EnemyMovement.cs
+++ Agent of Time/Assets/Scripts/EnemyMovement.cs
@@ -1,9 +1,14 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     [Header("Rigidbody")]
     [SerializeField] Rigidbody2D enemyRb2d;
+
+    [Header("Movement Settings")]
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] Rigidbody2D playerRb2d;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +19,7 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        enemyRb2d.transform.position = Vector2.MoveTowards(enemyRb2d.transform.position, playerRb2d.transform.position, moveSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
